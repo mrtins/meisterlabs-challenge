4363d06b88c1fe82ed2b1c82206a9eb886e5c06e
Vitor Martins <vitorfigmartins@gmail.com>
2022-04-20 20:35:44 +0100

    chore: setting up project

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Client from './pages/Client';
 import reportWebVitals from './reportWebVitals';
+import './styles/App.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Client />
   </React.StrictMode>
 );
 
